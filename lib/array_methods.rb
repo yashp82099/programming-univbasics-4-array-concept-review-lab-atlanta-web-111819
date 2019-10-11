6a8ca8942d176a398b749b1c6ac6bbc301f3c430
yash patel <yash82099@yahoo.com>
2019-10-11 20:33:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
     x = 0
-    while x < array.length do |array|
+    while x < array.length do
       puts array.index(value_to_find)
       x += 1 
     end 
